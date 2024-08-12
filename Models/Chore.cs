8da4f015c17c2156f2c7c2b840019cf523205b99
Evan Varozza <VarozzaEJ@gmail.com>
2024-08-12 15:41:45 -0600

    chore model

---
+++ Models/Chore.cs
@@ -0,0 +1,15 @@
+namespace chorescore.Models;
+
+public class Chores
+{
+public int Id {get; set;}
+
+public string Name {get; set;}
+
+public string Description {get; set;}
+
+public bool IsComplete {get; set;}
+
+public int TimeToComplete {get; set;}
+
+}
